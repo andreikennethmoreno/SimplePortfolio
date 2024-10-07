082e918488d49e6311b929e7bd84b6a5e08cd240
Kennonirom <Kennonirom@gmail.com>
2024-10-08 06:55:55 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "../assets/App.css"
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+import AboutSection from "./components/AboutSection";
+import FooterSection from "./components/FooterSection";
+import ProjectSection from "./components/ProjectSection";
+import EducationSection from "./components/EducationSection";
+import ContactSection from "./components/ContactSection";
+import SkillsSection from "./components/SkillsSection";
+
+
+function App () {
+  return (
+    <>
+      <NavBar/>
+      <HeroSection/>
+      <SkillsSection/>
+      <AboutSection/>
+      <ProjectSection />
+      <EducationSection/>
+      <ContactSection />
+      <FooterSection/>
+
+    </>
+  )
+}
+
+export default App;
